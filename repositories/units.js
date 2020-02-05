0c5e777b2ed93a67b5b882de9ad54dd1c8240db9
Grégoy Charmier <gregory.charmier@gmail.com>
2020-02-05 10:52:57 +0100

    [DPR] Make getUnitById deprecated

--- repositories/units.js
+++ repositories/units.js
@@ -4,11 +4,16 @@ module.exports = function (context) {
     
     let unitsRepo = {};
     let client = context.client;
-    
+
+    /* DEPRECATED: please use specific methods */
     unitsRepo.getUnitById = function (accountingNumber, next) {
+        unitsRepo.getUnitByAccountingNumber(accountingNumber, next);
+    };
+
+    unitsRepo.getUnitByAccountingNumber = function (accountingNumber, next) {
         client.executeQuery('(&(objectClass=organizationalunit)(|(accountingNumber=' + accountingNumber + ')))', unitFactory, context.options.modelsMapper.unit, true, next);
     };
-    
+
     unitsRepo.getUnitByUniqueIdentifier = function (unitId, next) {
         client.executeQuery('(&(objectClass=organizationalunit)(|(uniqueIdentifier=' + unitId + ')))', unitFactory, context.options.modelsMapper.unit, true, next);
     };
