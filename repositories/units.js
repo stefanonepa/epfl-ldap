d943c5a7f71431d6e102deba58724225785d8ed8
Nicolas Borboën <ponsfrilus@gmail.com>
2020-02-05 11:49:10 +0100

    [FIX] LDAP Filter "OR" not needed

--- repositories/units.js
+++ repositories/units.js
@@ -11,19 +11,19 @@ module.exports = function (context) {
     };
 
     unitsRepo.getUnitByAccountingNumber = function (accountingNumber, next) {
-        client.executeQuery('(&(objectClass=organizationalunit)(|(accountingNumber=' + accountingNumber + ')))', unitFactory, context.options.modelsMapper.unit, true, next);
+        client.executeQuery('(&(objectClass=organizationalunit)(accountingNumber=' + accountingNumber + '))', unitFactory, context.options.modelsMapper.unit, true, next);
     };
 
     unitsRepo.getUnitByUniqueIdentifier = function (unitId, next) {
-        client.executeQuery('(&(objectClass=organizationalunit)(|(uniqueIdentifier=' + unitId + ')))', unitFactory, context.options.modelsMapper.unit, true, next);
+        client.executeQuery('(&(objectClass=organizationalunit)(uniqueIdentifier=' + unitId + '))', unitFactory, context.options.modelsMapper.unit, true, next);
     };
 
     unitsRepo.getUnitByName = function (unit, next) {
-        client.executeQuery('(&(objectClass=organizationalunit)(|(ou=' + unit + ')))', unitFactory, context.options.modelsMapper.unit, true, next);
+        client.executeQuery('(&(objectClass=organizationalunit)(ou=' + unit + '))', unitFactory, context.options.modelsMapper.unit, true, next);
     };
     
     unitsRepo.searchUnitByName = function (unit, next) {
-        client.executeQuery('(&(objectClass=organizationalunit)(|(ou=*' + unit + '*)))', unitFactory, context.options.modelsMapper.unit, false, next);
+        client.executeQuery('(&(objectClass=organizationalunit)(ou=*' + unit + '*))', unitFactory, context.options.modelsMapper.unit, false, next);
     };
      
     return unitsRepo;
--- repositories/users.js
+++ repositories/users.js
@@ -6,42 +6,42 @@ module.exports = function (context) {
     let client = context.client;
     
     usersRepo.getUserBySciper = function (sciper, next) {
-        client.executeQuery('(&(objectClass=posixAccount)(|(uniqueIdentifier=' + sciper + ')))', userFactory, context.options.modelsMapper.user, true, next);
+        client.executeQuery('(&(objectClass=posixAccount)(uniqueIdentifier=' + sciper + '))', userFactory, context.options.modelsMapper.user, true, next);
     };
     
     usersRepo.getUserByName = function (name, next) {
-        client.executeQuery('(&(objectClass=posixAccount)(|(cn=' + name + ')))', userFactory, context.options.modelsMapper.user, false, next);
+        client.executeQuery('(&(objectClass=posixAccount)(cn=' + name + '))', userFactory, context.options.modelsMapper.user, false, next);
     };
     
     usersRepo.searchUserByName = function (name, next) {
-        client.executeQuery('(&(objectClass=posixAccount)(|(cn=' + name + '*)))', userFactory, context.options.modelsMapper.user, false, next);
+        client.executeQuery('(&(objectClass=posixAccount)(cn=' + name + '*))', userFactory, context.options.modelsMapper.user, false, next);
     };
     
     usersRepo.searchUserByPhone = function (phone, next) {
-        client.executeQuery('(&(objectClass=posixAccount)(|(telephoneNumber=*' + phone + '*)))', userFactory, context.options.modelsMapper.user, false, next);
+        client.executeQuery('(&(objectClass=posixAccount)(telephoneNumber=*' + phone + '*))', userFactory, context.options.modelsMapper.user, false, next);
     };
     
     usersRepo.searchUserByUnitAcronym = function (unitAcronym, next) {
-        client.executeQuery('(&(objectClass=posixAccount)(|(ou=' + unitAcronym + ')))', userFactory, context.options.modelsMapper.user, false, next);
+        client.executeQuery('(&(objectClass=posixAccount)(ou=' + unitAcronym + '))', userFactory, context.options.modelsMapper.user, false, next);
     };
 
     /* The plural forms always return a list, regarding of whether the search
      * criterion is unique.
      */
     usersRepo.getUsersBySciper = function (sciper, next) {
-        client.executeQuery('(&(objectClass=posixAccount)(|(uniqueIdentifier=' + sciper + ')))', userFactory, context.options.modelsMapper.user, false, next);
+        client.executeQuery('(&(objectClass=posixAccount)(uniqueIdentifier=' + sciper + '))', userFactory, context.options.modelsMapper.user, false, next);
     };
 
     usersRepo.getUsersByName = function (name, next) {
-        client.executeQuery('(&(objectClass=posixAccount)(|(cn=' + name + ')))', userFactory, context.options.modelsMapper.user, false, next);
+        client.executeQuery('(&(objectClass=posixAccount)(cn=' + name + '))', userFactory, context.options.modelsMapper.user, false, next);
     };
 
     usersRepo.getUsersByPhone = function (phone, next) {
-        client.executeQuery('(&(objectClass=posixAccount)(|(telephoneNumber=*' + phone + '*)))', userFactory, context.options.modelsMapper.user, false, next);
+        client.executeQuery('(&(objectClass=posixAccount)(telephoneNumber=*' + phone + '*))', userFactory, context.options.modelsMapper.user, false, next);
     };
 
     usersRepo.getUsersByUnitAcronym = function (unitAcronym, next) {
-        client.executeQuery('(&(objectClass=posixAccount)(|(ou=' + unitAcronym + ')))', userFactory, context.options.modelsMapper.user, false, next);
+        client.executeQuery('(&(objectClass=posixAccount)(ou=' + unitAcronym + '))', userFactory, context.options.modelsMapper.user, false, next);
     };
   
     return usersRepo;
