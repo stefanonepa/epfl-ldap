4a068e6fa52cdc21b04cc016e09e4e777bc16217
Nicolas Borboën <ponsfrilus@gmail.com>
2020-02-12 10:24:51 +0100

    [FT.] ByMail method + better phone handling
    * Now one can query the ldap using user's email
    * Querying with phone will now handle "12345" and "+41 21 6912345"

--- test/userTest.js
+++ test/userTest.js
@@ -20,7 +20,7 @@ describe('User::get', function () {
 
     it('getUserBySciper should return correct email length', function (done) {
         fullLdapContext.users.getUserBySciper(169419, function (err, data) {
-            assert.ok(data.emails[0].length === 23, "email adresse is long");
+            assert.ok(data.emails[0].length === 23, "Email adresse has the correct length");
             done();
         });
     });
@@ -28,6 +28,28 @@ describe('User::get', function () {
     it('getUserByName should find Kermit La Grenouille', function (done) {
         fullLdapContext.users.getUserByName('Kermit La Grenouille', function (err, data) {
             assert.equal(data.displayName, 'Kermit La Grenouille');
+            done();
+        });
+    });
+
+    it('getUserByPhone should find 169419', function (done) {
+        fullLdapContext.users.getUserByPhone('+41 21 6935455', function (err, data) {
+            assert.equal(data.sciper, '169419');
+            done();
+        });
+    });
+
+    it('getUserByPhone should find 169419', function (done) {
+        fullLdapContext.users.getUserByPhone('35455', function (err, data) {
+            assert.equal(data.sciper, '169419');
+            done();
+        });
+    });
+
+    // Watch out: user with guest account on their EPFL email can appear first.
+    it('getUserByMail should find 188475', function (done) {
+        fullLdapContext.users.getUserByMail('gregory.charmier@epfl.ch', function (err, data) {
+            assert.equal(data.sciper, '188475');
             done();
         });
     });
@@ -82,6 +104,14 @@ describe('Users::get', function () {
 
     it('getUsersByPhone should get 169419', function (done) {
         fullLdapContext.users.getUsersByPhone('35455', function (err, data) {
+            // Note: it should be an array...
+            assert.equal(data.sciper, '169419');
+            done();
+        });
+    });
+
+    it('getUsersByMail should find 169419', function (done) {
+        fullLdapContext.users.getUsersByMail('nicolas.borboen@epfl.ch', function (err, data) {
             assert.equal(data[0].sciper, '169419');
             done();
         });
