e7d00c8b811c32ef66e149a11198f7fa7667cf40
Nicolas Borboën <ponsfrilus@gmail.com>
2020-02-12 12:57:39 +0100

    [FIX] WARNING! node-cache legacy callback support will drop in v6.x
    Close #3
    
    * Get rid of cache callbacks
    * Use the same context for all the tests

--- test/init.js
+++ test/init.js
@@ -1,11 +1,13 @@
 before(function() {
-  //console.log('before test in every file');
+  // console.log('before test in every file');
+  var fullLdapContext = require('../context')();
+  fullLdapContext.options.modelsMapper = fullLdapContext.viewModelsMappers.full;
+  // create a context for all the test, allowing to create/remove cache
+  this.fullLdapContext = fullLdapContext;
 });
-
 
 beforeEach(function() {
   // Be sure to clear the cache before the tests...
   // console.log('clearing cache...');
-  var fullLdapContext = require('../context')();
-  fullLdapContext.memoryCache.flushAll();
+  this.fullLdapContext.memoryCache.flushAll();
 });
--- test/unitTest.js
+++ test/unitTest.js
@@ -1,32 +1,30 @@
 var assert = require('assert');
-var fullLdapContext = require('../context')();
-fullLdapContext.options.modelsMapper = fullLdapContext.viewModelsMappers.full;
 
 describe('Units::get', function () {
 
     it('get unit by id', function(done) {
-        fullLdapContext.units.getUnitById('1906', function (err, data) {
+        this.fullLdapContext.units.getUnitById('1906', function (err, data) {
             assert.ok(data.name === "SI - Full-Stack Development", "Unit name");
             done();
         });
     });
-  
+
     it('get unit by accounting number', function (done) {
-        fullLdapContext.units.getUnitByAccountingNumber('1906', function (err, data) {
+        this.fullLdapContext.units.getUnitByAccountingNumber('1906', function (err, data) {
             assert.ok(data.name === "SI - Full-Stack Development", "Unit name");
             done();
         });
     });
- 
+
     it('get unit by unique identifier', function (done) {
-        fullLdapContext.units.getUnitByUniqueIdentifier(13030, function (err, data) {
+        this.fullLdapContext.units.getUnitByUniqueIdentifier(13030, function (err, data) {
             assert.ok(data.name === "SI - Full-Stack Development", "Unit name");
             done();
         });
     });
-  
+
     it('get unit by name', function (done) {
-        fullLdapContext.units.getUnitByName("SI - Full-Stack Development", function (err, data) {
+        this.fullLdapContext.units.getUnitByName("SI - Full-Stack Development", function (err, data) {
             assert.ok(data.uniqueIdentifier === '13030', "Unit unique identifier");
             done();
         });
@@ -37,7 +35,7 @@ describe('Units::get', function () {
 describe('Units::search', function () {
 
     it('search unit by name', function (done) {
-        fullLdapContext.units.searchUnitByName("Full-Stack Development", function (err, data) {
+        this.fullLdapContext.units.searchUnitByName("Full-Stack Development", function (err, data) {
             assert.ok(data[0].uniqueIdentifier === '13030', "Unit unique identifier");
             done();
         });
